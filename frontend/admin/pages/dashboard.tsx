88ed191394a237676860b9341d322c184c3fcbc4
Nathanael DCunha <nathanael.dcunha@gmail.com>
2025-03-04 23:29:19 +0530

    chore: initial dashboard page

--- frontend/admin/pages/dashboard.tsx
+++ frontend/admin/pages/dashboard.tsx
@@ -1,3 +1,22 @@
+import { Head } from '@inertiajs/react';
+import { AdminLayout } from '../layout';
+import { Card } from '../components/ui/card';
+
 export default function Dashboard() {
-	return <h1>This is a dashboard</h1>;
+	return (
+		<>
+			<Head title="Dashboard" />
+			<div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+				<Card></Card>
+				<Card></Card>
+				<Card></Card>
+				<Card></Card>
+			</div>
+			<Card></Card>
+		</>
+	);
 }
+
+Dashboard.layout = (page: React.ReactElement) => (
+	<AdminLayout className="space-y-8">{page}</AdminLayout>
+);
