dfa8f41c484707c211e15982331ea60e0ca80abc
Nathanael DCunha <nathanael.dcunha@gmail.com>
2025-03-05 10:18:24 +0530

    build: switch from tailwindcss vite to postcss

--- vite.admin.config.js
+++ vite.admin.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
@@ -14,7 +13,6 @@ export default defineConfig({
 			},
 		}),
 		react(),
-		tailwindcss(),
 	],
 	resolve: {
 		alias: {
--- vite.web.config.js
+++ vite.web.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
@@ -14,7 +13,6 @@ export default defineConfig({
 			},
 		}),
 		react(),
-		tailwindcss(),
 	],
 	resolve: {
 		alias: {
