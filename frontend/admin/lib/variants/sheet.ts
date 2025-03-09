c6847f7e7c94ed1539824c48ba543b044eafbb97
Nathanael DCunha <nathanael.dcunha@gmail.com>
2025-03-09 16:52:13 +0530

    feat: removed comments

--- frontend/admin/lib/variants/sheet.ts
+++ frontend/admin/lib/variants/sheet.ts
@@ -1,15 +1,5 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
-// 'fixed z-50 flex flex-col gap-4 bg-background shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=closed]:animate-out data-[state=open]:duration-500 data-[state=open]:animate-in',
-// 					side === 'right' &&
-// 						'inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
-// 					side === 'left' &&
-// 						'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
-// 					side === 'top' &&
-// 						'inset-x-0 top-0 h-auto border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
-// 					side === 'bottom' &&
-// 						'inset-x-0 bottom-0 h-auto border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
-
 const sheetVariants = cva(
 	'fixed z-50 flex flex-col shadow-lg transition open:animate-in focus-visible:outline-none closed:animate-out',
 	{
