a4c156961c9c7563870a674fac28ef1658b5e929
Nathanael DCunha <nathanael.dcunha@gmail.com>
2025-03-09 16:52:39 +0530

    feat(route): added error fallback route

--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\GenreController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\SeriesController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::domain('admin.' . config('app.url'))->name('admin.')->group(function () {
 	Route::controller(HomeController::class)->group(function () {
@@ -27,5 +28,9 @@ Route::domain('admin.' . config('app.url'))->name('admin.')->group(function () {
 
 	Route::controller(GenreController::class)->prefix('genres')->group(function () {
 		Route::get('/', 'index')->name('genres.index');
+	});
+
+	Route::fallback(function () {
+		return Inertia::render('404');
 	});
 });
